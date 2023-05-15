94127546ea853c99db5e5c6deeea451c0e0133e1
rohann8334 <128222478+rohann8334@users.noreply.github.com>
2023-05-15 19:50:44 +0530

    Add files via upload

---
+++ 100.sql
@@ -0,0 +1,25 @@
+select * from "SBA_national";
+select ("DisbursementGross") from "SBA_national";
+select "NAICS" from "SBA_national";
+
+select MAX("NAICS")from "SBA_national";
+
+select "NAICS" from "SBA_national";
+
+select * from "SBA_national";
+
+select SUM("UrbanRural") from "SBA_national";
+
+select MAX("UrbanRural") from "SBA_national";
+
+select "UrbanRural" from "SBA_national";
+
+select MAX("NewExist") from "SBA_national";
+
+select DISTINCT("NewExist") from "SBA_national";
+
+select * from "SBA_national";
+select max("DisbursementGross") from "SBA_national";
+
+
+
